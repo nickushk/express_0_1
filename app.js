8643a8ca149e600763f06ffbb6b6d4d3af715a95
Nick Kushkbaghi <nick.kushkbaghi@gmail.com>
2021-11-17 23:30:20 +0100

    test

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+var port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send("Hello Word");
+});
+app.listen(port);
